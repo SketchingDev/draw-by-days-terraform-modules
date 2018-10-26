f4b280371cfbbcd87c4e3bf8e15e8ffe5e95ed23
SketchingDev <SketchingDev@users.noreply.github.com>
2018-10-26 14:27:36 +0100

    Correct output name for s3 zone ID

--- s3_website/outputs.tf
+++ s3_website/outputs.tf
@@ -6,7 +6,7 @@ output "url" {
     value = "${aws_s3_bucket.website.website_endpoint}"
 }
 
-output "hosted_zone_ids" {
+output "hosted_zone_id" {
     sensitive = true,
     value = "${aws_s3_bucket.website.hosted_zone_id}"
 }
