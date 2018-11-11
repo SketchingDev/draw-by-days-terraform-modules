790827ace20111a42ec18d977192a6a6a1e2cd75
SketchingDev <SketchingDev@users.noreply.github.com>
2018-11-11 22:29:18 +0000

    Create DynamoDB API Gateway

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 Reusable Terraform modules for [Draw-By-Days](). They have been moved into their own repository
 to:
  * **Save me money** - the tests run against AWS on every commit
- * **Simplify configuration** - Terragrunt only supports absolute paths, so [modules cannot easily 
+ * **Simplify configuration** - Terragrunt only supports absolute paths, so [modules cannot easily
  reference other modules](https://community.gruntwork.io/t/relative-paths-in-terragrunt-modules/144/6). By
  moving them out into their own repository I can create service modules that reference multiple modules
  using their URLS. Also Terragrunt cannot reference [multiple modules](https://github.com/gruntwork-io/terragrunt/issues/350). See [xyz]() for an example.
@@ -16,4 +16,9 @@ to:
 ```
 cd draw-by-days-terraform-modules
 docker run -it -v $(pwd)/lambda_api_gateway/:/go/src/app sketchingdev/golang-terratest:latest
+
+cd test
+dep init
+dep ensure -v
+go test -v -run TestApiGatewayReturnsLambdaResponse
 ```
---
+++ readonly_dynamodb_api_gateway/test/terraform_http_test.go
@@ -0,0 +1,115 @@
+package test
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"testing"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+
+	"github.com/gruntwork-io/terratest/modules/http-helper"
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestApiGatewayReturnsEmptyDynamoDBResponse(t *testing.T) {
+	t.Parallel()
+
+	awsRegion := "us-east-1" //:= aws.GetRandomRegion(t, nil, nil)
+
+	uniqueID := random.UniqueId()
+	tableName := fmt.Sprintf("terratest-dynamodb-table-%s", uniqueID)
+
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(awsRegion)},
+	)
+
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
+
+	input := &dynamodb.CreateTableInput{
+		AttributeDefinitions: []*dynamodb.AttributeDefinition{
+			{
+				AttributeName: aws.String("testId"),
+				AttributeType: aws.String("S"),
+			},
+		},
+		KeySchema: []*dynamodb.KeySchemaElement{
+			{
+				AttributeName: aws.String("testId"),
+				KeyType:       aws.String("HASH"),
+			},
+		},
+		ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
+			ReadCapacityUnits:  aws.Int64(1),
+			WriteCapacityUnits: aws.Int64(1),
+		},
+		TableName: aws.String(tableName),
+	}
+
+	deleteTableInput := &dynamodb.DeleteTableInput{
+		TableName: aws.String(tableName),
+	}
+
+	tableOutput, err := svc.CreateTable(input)
+
+	if err != nil {
+		fmt.Println("Got error calling CreateTable:")
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	tableArn := *tableOutput.TableDescription.TableArn
+
+	// Given
+
+	requestTemplate := fmt.Sprintf(`{
+        \"TableName\": \"%s\",
+        \"KeyConditionExpression\": \"testId = :v1\",
+        \"ExpressionAttributeValues\": {
+            \":v1\": {
+                \"S\": \"$input.params('testId')\"
+            }
+        }
+    }`, tableName)
+	re := regexp.MustCompile(`\r?\n?`)
+	requestTemplate = re.ReplaceAllString(requestTemplate, "")
+
+	namespace := fmt.Sprintf("terratest-dynamodb-test-%s", uniqueID)
+
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../",
+
+		Vars: map[string]interface{}{
+			"namespace":        namespace,
+			"stage_name":       "test",
+			"table_arn":        tableArn,
+			"query_path":       "{testId}",
+			"request_template": requestTemplate,
+		},
+
+		EnvVars: map[string]string{
+			"AWS_DEFAULT_REGION": awsRegion,
+		},
+	}
+
+	defer terraform.Destroy(t, terraformOptions)
+	defer svc.DeleteTable(deleteTableInput)
+
+	// When
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Then
+	instanceURL := terraform.OutputRequired(t, terraformOptions, "url")
+	instanceURL = fmt.Sprintf("%s/test-value", instanceURL)
+
+	expected := "{\"Count\":0,\"Items\":[],\"ScannedCount\":0}"
+	maxRetries := 10
+	timeBetweenRetries := 5 * time.Second
+
+	http_helper.HttpGetWithRetry(t, instanceURL, 200, expected, maxRetries, timeBetweenRetries)
+}
