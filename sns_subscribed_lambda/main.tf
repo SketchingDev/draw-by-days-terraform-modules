6338a939788999fa4d70e78a41834b5574441326
SketchingDev <31957045+SketchingDev@users.noreply.github.com>
2018-12-27 19:44:30 +0000

    Replace Lambda policy for logging with AWS managed policy

--- sns_subscribed_lambda/main.tf
+++ sns_subscribed_lambda/main.tf
@@ -48,29 +48,7 @@ resource "aws_iam_role" "lambda_exec" {
 EOF
 }
 
-resource "aws_iam_policy" "lambda_logging" {
-  name = "${var.namespace}_lambda_logging"
-  path = "/"
-  description = "IAM policy for logging from a lambda"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],
-      "Resource": "arn:aws:logs:*:*:*",
-      "Effect": "Allow"
-    }
-  ]
-}
-EOF
-}
-
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
   role = "${aws_iam_role.lambda_exec.name}"
-  policy_arn = "${aws_iam_policy.lambda_logging.arn}"
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
 }
