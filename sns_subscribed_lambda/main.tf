0215949411decb719321547fb4b9367d9072b0f0
Lucas <31957045+SketchingDev@users.noreply.github.com>
2019-01-16 21:50:01 +0000

    Fix Lambda logging (#12)
    * Replace Lambda policy for logging with AWS managed policy
    * Create log-group for SNS Lambda

--- sns_subscribed_lambda/main.tf
+++ sns_subscribed_lambda/main.tf
@@ -48,29 +48,12 @@ resource "aws_iam_role" "lambda_exec" {
 EOF
 }
 
-resource "aws_iam_policy" "lambda_logging" {
-  name = "${var.namespace}_lambda_logging"
-  path = "/"
-  description = "IAM policy for logging from a lambda"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],
-      "Resource": "arn:aws:logs:*:*:*",
-      "Effect": "Allow"
-    }
-  ]
-}
-EOF
+resource "aws_cloudwatch_log_group" "sns_lambda_log_group" {
+  name              = "/aws/lambda/${aws_lambda_function.sns_subscriber.function_name}"
+  retention_in_days = 14
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
   role = "${aws_iam_role.lambda_exec.name}"
-  policy_arn = "${aws_iam_policy.lambda_logging.arn}"
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
 }
